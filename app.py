72a3293a1937d81e9d86039cefa080b534703d8c
elvinsanchez0409-star <elvinsanchez0409@gmail.com>
2026-08-14 18:10:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1097,7 +1097,7 @@ def modulo(name):
     if not modulo_actual:
         return redirect("/")
 
-        botones = ""
+           botones = ""
 
     for option in modulo_actual["opciones"]:
         botones += f"""
